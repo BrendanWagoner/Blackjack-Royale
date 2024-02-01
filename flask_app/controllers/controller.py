4555f3e06b3d218287b17968e9fbb62dfe659f77
Brendan Wagoner <schadohbjw@gmail.com>
2024-02-01 01:21:55 -0800

    added login, validation, users, crud and more

--- flask_app/controllers/controller.py
+++ flask_app/controllers/controller.py
@@ -1,8 +1,63 @@
 from flask import Flask, render_template, redirect, request, session, flash, jsonify
 from flask_app import app
+from flask_bcrypt import Bcrypt
+from flask_app.models import user
+import os, re
+
+bcrypt = Bcrypt(app)
+CLEANR = re.compile('<.*?>')
 
 @app.route('/')
 @app.route('/play')
 def play():
 
     return render_template('table.html')
+
+@app.route('/<int:id>/play')
+def render_user_table(id):
+    
+    return render_template('user_table.html', u=user.User.get_user({'id':id}))
+
+@app.route('/login')
+def login_index():
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    print(request.form)
+
+    if not user.User.validate_user(request.form):
+        return redirect('/login')
+    
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+
+    user_id = user.User.create_user({'username': request.form['username'], 'email': request.form['email'], 'password': pw_hash})
+
+    if 'user_id' not in session:
+        session['user_id'] = user_id
+    session['user_id'] = user_id
+
+    return redirect(f'/{user_id}/play')
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    u = user.User.get_user_by_email({'email': request.form['user-email']})
+
+    if not u:
+        flash('invalid email or password', 'login_error')
+        return redirect('/login')
+    if not bcrypt.check_password_hash(u.password, request.form['user-password']):
+        flash('invalid password, please try again.', 'login_error')
+        return redirect('/login')
+    
+    session['user_id'] = u.id
+
+    return redirect(f'/{u.id}/play')
+
+# TODO update user username and coin after logging out 
+@app.route('/logout')
+def logout():
+    session.clear()
+
+    return redirect('/play')
---
+++ flask_app/models/user.py
@@ -0,0 +1,105 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class User:
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.username = data['username']
+        self.email = data['email']
+        self.password = data['password']
+        self.coins = data['coins']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        
+    # create
+    @classmethod
+    def create_user(cls, data):
+        sql = '''INSERT INTO users(username, email, password, coins, created_at, updated_at)
+        VALUES(%(username)s, %(email)s, %(password)s, 300, NOW(), NOW());'''
+        results = connectToMySQL('twentyone').query_db(sql, data)
+
+        return results
+
+    # read
+    @classmethod
+    def get_user(cls, data):
+        sql = 'SELECT * FROM users WHERE id=%(id)s;'
+        results = connectToMySQL('twentyone').query_db(sql, data)
+        
+        return cls(results[0])
+    
+    @classmethod
+    def get_user_by_email(cls, data):
+        sql = 'SELECT * FROM users WHERE email=%(email)s;'
+        results = connectToMySQL('twentyone').query_db(sql, data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    # update 
+    @classmethod
+    def update_user(cls, data):
+        sql = '''UPDATE users
+        set username=%(username)s, email=%(email)s, password=%(password)s, coins=%(coins)s, updated_at=NOW()
+        WHERE id=%(id)s;'''
+
+        return connectToMySQL('twentyone').query_db(sql, data)
+    
+    # delete
+    @classmethod
+    def delete_user(cls, data):
+        sql = 'DELETE FROM users WHERE id=%(id)s;'
+        result = connectToMySQL('twentyone').query_db(sql, data)
+
+    #validation
+    
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        if len(user['username']) < 2:
+            flash('Username must be at least 2 characters long', 'registration_error')
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash('Please enter a valid email', 'registration_error')
+            is_valid = False
+        if len(user['password']) < 8:
+            flash('Password must be at least 8 characters long', 'registration_error')
+            is_valid = False
+        if User.special_character_count(user['password']) < 1:
+            flash('Password needs at least one uppercase character', 'registration_error')
+            is_valid = False
+        if User.digit_count(user['password']) < 1:
+            flash('Password needs at least one number', 'registration_error')
+            is_valid = False
+        if user['confirm'] != user['password']:
+            flash('confirmation password and password did not match', 'registration_error')
+            is_valid = False
+        if User.get_user_by_email({'email':user['email']}) != False:
+            flash('email already in use')
+            is_valid = False
+
+        return is_valid
+    
+    @staticmethod
+    def special_character_count(string):
+        counter = 0
+        for char in string:
+            if char.isupper():
+                counter +=1
+        
+        return counter
+    
+    @staticmethod
+    def digit_count(string):
+        counter = 0
+        for char in string:
+            if char.isdigit():
+                counter += 1
+
+        return counter
+
+
